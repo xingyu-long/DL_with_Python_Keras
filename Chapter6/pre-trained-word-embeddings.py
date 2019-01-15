9e5e4cefe95a5b028db3fcacea6b864b592030af
halolong <halolong@hotmail.com>
2019-01-15 13:50:16 +0800

    use pre-trained word embeddings
    Signed-off-by: halolong <halolong@hotmail.com>

---
+++ Chapter6/pre-trained-word-embeddings.py
@@ -0,0 +1,24 @@
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+import numpy as np
+import os
+
+imdb_dir = '/home/xingyu/Downloads'
+train_dir = os.path.join(imdb_dir, 'train')
+
+labels = []
+texts = []
+
+for label_type in ['neg', 'pos']:
+    dir_name = os.path.join(train_dir, label_type)
+    for fname in os.listdir(dir_name):
+        if fname[-4:] == '.txt':
+            f = open(os.path.join(dir_name, fname))
+            texts.append(f.read())
+            f.close()
+            if label_type == 'neg':
+                labels.append(0)
+            else:
+                labels.append(1)
+
+# ------- NOT FINISHED YET-----------------
